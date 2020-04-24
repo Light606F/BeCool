8fa42919c4cf4513004046706e577679029daa04
Light606F <Light606F.tsukineko@gmail.com>
2020-04-25 01:29:54 +0900

    Made charctor movable

--- main.py
+++ main.py
@@ -7,30 +7,51 @@ TileSize = 8
 class obj():
 	"""docstring for obj."""
 
-	def __init__(self, tileX, tileY):
+	def __init__(self):
 		self.x = 0
 		self.y = 0
-		self.tileX = tileX
-		self.tileY = tileY
+		self._setTile(0,0)
+
+	def _setTile(self, x, y):
+		self.tileX = x
+		self.tileY = y
 
 
 class Charactor(obj):
 	"""docstring for Charactor."""
-	pass
+
+	def __init__(self):
+		super().__init__()
+		self.y = DisplayHeight-TileSize*2
+		self._setTile(0, 16)
+
+	def update(self):
+		if pyxel.btn(pyxel.KEY_UP):
+			self.y -= 2
+
+		if pyxel.btn(pyxel.KEY_RIGHT):
+			self.x = min( self.x+2, DisplayWidth-TileSize )
+
+		if pyxel.btn(pyxel.KEY_LEFT):
+			self.x = max( self.x-2, DisplayWidth-TileSize )
+
+		if self.y < DisplayHeight-TileSize*2:
+			self.y +=1
 
 
 class Floor(obj):
 	"""docstring for Charactor."""
 
-	def __init__(self, tileX, tileY):
-		super().__init__(tileX, tileY)
+	def __init__(self):
+		super().__init__()
 		self.y = DisplayHeight-TileSize
+		self._setTile(8, 0)
 
 
 class App:
 	def __init__(self):
-		self.playerChr = Charactor(0, 16)
-		self.floor = Floor(8, 0)
+		self.playerChr = Charactor()
+		self.floor = Floor()
 
 		pyxel.init(DisplayWidth, DisplayHeight, caption="Zero One")
 		pyxel.load("assets/assets.pyxres")
@@ -41,8 +62,7 @@ class App:
 		if pyxel.btnp(pyxel.KEY_Q):
 			pyxel.quit()
 
-		def updatePlayer():
-			pass
+		self.playerChr.update()
 
 	def draw(self):
 		pyxel.cls(7)
